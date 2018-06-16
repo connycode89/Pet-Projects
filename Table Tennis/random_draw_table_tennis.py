c08ad1ea0228e4ca84c1a0c21a228c2e87a604ac
connycode89 <32690940+connycode89@users.noreply.github.com>
2018-06-16 01:39:36 +0100

    Full Program

--- Table Tennis/random_draw_table_tennis.py
+++ Table Tennis/random_draw_table_tennis.py
@@ -9,6 +9,7 @@ Created on Fri Jun 15 13:40:29 2018
 # code to do a random draw to put teams into n number of groups
 import pandas as pd
 from math import floor
+import numpy as np
 
 def read_data(path):
     teams = pd.read_csv(path)
@@ -24,6 +25,23 @@ def determine_format(num_groups, num_teams):
         second_groups = [base_amt_per_grp for num in range(1, num_groups-remainder+1)]
         return first_groups+second_groups
     
-determine_format(5,12)
+def randomize(team_col):
+    list1 = list(team_col)
+    np.random.shuffle(list1)
+    return list1
+    
+def draw(path1, n_groups):
+    col1 = read_data(path1)
+    format1 = determine_format(n_groups, len(col1))
+    rearrange = randomize(col1)
+    draw_list = []
+    for item in format1:
+        first = col1[:item]
+        draw_list.append(first)
+        col1 = [item for item in col1 if item not in first]
+    return draw_list
+    
 
-path = 'C:\\Users\\cdonovan\\Downloads\\Table Tennis Registration.csv.zip'
+#path = 'C:\\Users\\cdonovan\\Downloads\\Table Tennis Registration.csv.zip'
+path = 'C:\\Users\\Conor\\Downloads\\Table Tennis Registration.csv.zip'
+results = draw(path, 2)
